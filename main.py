ca1205da32dcb403c4222c2a88938005800a9be1
Kyle Twombley <ktwom22@gmail.com>
2025-09-15 20:42:23 -0400

    fix

--- main.py
+++ main.py
@@ -243,6 +243,5 @@ def lineups():
 
 import os
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
